7d52e88bf45f4e69e5a886b311f71086d05704aa
MartSlaaf <mart.slaaf@gmail.com>
2019-07-12 12:53:10 +0000

    [*] non-serious changes

--- beautylogger/beautylogger.py
+++ beautylogger/beautylogger.py
@@ -10,7 +10,6 @@ from tqdm import tqdm
 
 class BeautyLogger:
     #TODO: add comments
-    #TODO: refactor holding of plottables to let user select only name, not train\test
     #TODO: add more conveniency in calculation and agregation
     #TODO: add overridable aggregators?
     #TODO: add tests
