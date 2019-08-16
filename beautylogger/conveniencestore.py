cc2691b79e81fa8171903630a9671aa0d9a4d01d
Yaroslav Zharov <mart.slaaf@gmail.com>
2019-08-16 20:04:49 +0200

    [+] First experiments with convenience_store -- set of convenience functions for life

---
+++ beautylogger/conveniencestore.py
@@ -0,0 +1,36 @@
+from IPython.core.display import HTML
+
+
+def get_color_code(value, coloring_type):
+    if coloring_type == 'positive':
+        r = 0
+        g = value * 255
+        b = 0
+    elif coloring_type == 'positive-negative':
+        r = (1 - value) * 255
+        g = value * 255
+        b = 0
+    elif coloring_type == 'cathegory':
+        raise NotImplementedError('colorisation with cathegory id is not implemented yet!')
+    else:
+        raise Exception('Unknown coloring type "{coloring_type}"')
+    return r, g, b
+
+
+from cgi import escape
+
+def decorate_text_with_words(text, intensity, inverse_dictionary=None, coloring_type='positive'):
+
+    if len(text) != len(intensity):
+        raise ValueError('Non-coherent lengths of text and intensities')
+
+    if inverse_dictionary is not None:
+        text = [inverse_dictionary[w] for w in text]
+
+    html_result = ''
+    for t, i in zip(text, intensity):
+        r, g, b = get_color_code(i)
+        t = escape(t)
+        seq_diane.append(f'<span style="background-color: rgb({r}, {g}, {b}">{t} </span>')
+    fin_html = ''.join(seq_diane)
+    display(HTML(fin_html))
